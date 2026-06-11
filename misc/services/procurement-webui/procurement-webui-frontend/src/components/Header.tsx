948c18513eee68e9051dbc0805de4d1d2afbc9a1
snjrbkv <odilbeksarvarbekovich@gmail.com>
2026-06-11 11:35:42 +0500

    commit

--- misc/services/procurement-webui/procurement-webui-frontend/src/components/Header.tsx
+++ misc/services/procurement-webui/procurement-webui-frontend/src/components/Header.tsx
@@ -6,37 +6,37 @@ import { translate } from '../utils/translate';
 import { RootInstance } from '../models/Store';
 
 interface Props {
-    store?: RootInstance;
+  store?: RootInstance;
 }
 
 const Header: FunctionComponent<Props> = inject('store')(
-    observer(({ store }): ReactElement => {
-        const location = useLocation();
-        let fakeAligner = null;
-        let link = null;
+  observer(({ store }): ReactElement => {
+    const location = useLocation();
+    let fakeAligner = null;
+    let link = null;
 
-        if (location.pathname === '/') {
-            fakeAligner = <div className="header-aligner" />;
-            link = (
-                <div className="logout-link">
-                    <a onClick={store.app.logOut} className="button is-success is-green-bg">
-                        {translate('Logout.caption')}
-                    </a>
-                </div>
-            );
-        }
+    if (location.pathname === '/') {
+      fakeAligner = <div className="header-aligner" />;
+      link = (
+        <div className="logout-link">
+          <a onClick={store.app.logOut} className="button is-success is-green-bg">
+            {translate('Logout.caption')}
+          </a>
+        </div>
+      );
+    }
 
-        return (
-            <header className="p-4 navbar is-fixed-top is-flex is-align-items-center is-justify-content-space-around header">
-                <h1>somsa</h1>
-                {fakeAligner}
-                <div className="header-title">
-                    <h4 className="title is-4">{store.navigation.topViewName}</h4>
-                </div>
-                {link}
-            </header>
-        );
-    }),
+    return (
+      <header className="p-4 navbar is-fixed-top is-flex is-align-items-center is-justify-content-space-around header">
+        <h1>somsa</h1>
+        {fakeAligner}
+        <div className="header-title">
+          <h4 className="title is-4">{store.navigation.topViewName}</h4>
+        </div>
+        {link}
+      </header>
+    );
+  })
 );
 
 export default Header;
